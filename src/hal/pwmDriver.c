06603e035be98e300d9355b690ae3d4374a74522
Miguel Parra <migparra73@gmail.com>
2023-04-04 15:54:50 -0700

    added suggestions to PWM code

--- src/hal/pwmDriver.c
+++ src/hal/pwmDriver.c
@@ -4,5 +4,9 @@
 int PwmDriver_Init(void)
 {
     int *driver;
+
+    TC0_REGS->COUNT8.TC_CTRLA; // Ctrl registers Set A 
+    TC0_REGS->COUNT8.TC_CTRLB; // Ctrl registers Set B
+    TC0_REGS->COUNT8.TC_WAVE; // Waveform Generation Control
     // Something  helllo 
 }
